8cf7925b2b45a897ccc58a21fcf7c40342ba206f
Wei-Yu Chen <aweimeow.tw@gmail.com>
2017-12-26 13:04:26 +0800

    Update git automatic checkout to cord-4.1

--- start.sh
+++ start.sh
@@ -1,19 +1,34 @@
-#! /usr/bin/bash
+#!/bin/bash
 
-PATH=~/cord/build
+DST=~/cord/build
 TMP=/tmp/oai_scenario_backup/
 mkdir /tmp/oai_scenario_backup
 
 # Backup all old files
-cp $PATH/docker_images.yml $TMP
-cp $PATH/../.repo/manifests/default.xml $TMP
-cp $PATH/platform-install/roles/cord-profile/templates/public-net.yaml.j2 $TMP
+cp $DST/docker_images.yml $TMP
+cp $DST/../.repo/manifests/default.xml $TMP
+cp $DST/platform-install/roles/cord-profile/templates/public-net.yaml.j2 $TMP
 
 # Copy new files into our target
-cp docker_images.yml $PATH/
-cp manifest.xml $PATH/../.repo/manifests/default.xml
-cp mcord-oai.yml $PATH/platform-install/profile_manifests/
-cp mcord-oai-virtual.yml $PATH/podconfig/
-cp public-net.yaml.j2 $PATH/platform-install/roles/cord-profile/templates/
-cp oai-net.yaml.j2 $PATH/platform-install/roles/cord-profile/templates/
-cp mcord-oai-services.yml.j2 $PATH/platform-install/roles/cord-profile/templates
+cp docker_images.yml $DST/
+cp manifest.xml $DST/../.repo/manifests/default.xml
+cp mcord-oai.yml $DST/platform-install/profile_manifests/
+cp mcord-oai-virtual.yml $DST/podconfig/
+cp public-net.yaml.j2 $DST/platform-install/roles/cord-profile/templates/
+cp oai-net.yaml.j2 $DST/platform-install/roles/cord-profile/templates/
+cp mcord-oai-services.yml.j2 $DST/platform-install/roles/cord-profile/templates
+
+# Use custom version of vhss, vmme instead official
+cd ~/cord/orchestration/xos_services
+rm -rf vhss vmme
+git clone https://github.com/aweimeow/vMME vmme
+git clone https://github.com/aweimeow/vHSS vhss
+git clone https://github.com/aweimeow/oaispgw oaispgw
+
+# Checkout to target branch
+for var in "vmme" "vhss" "oaispgw"; do
+    cd $var;
+    git checkout cord-4.1;
+    git pull;
+    cd ..;
+done
