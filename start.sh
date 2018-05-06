eef8a43207bedf8c94fe6898a7e1139ceda2f509
Wei-Yu Chen <aweimeow.tw@gmail.com>
2018-05-06 11:23:21 +0800

    Done Testing on cord-5.0 CORD-POD ver.

--- start.sh
+++ start.sh
@@ -6,13 +6,13 @@ DST=~/cord
 
 # Copy new files into our target
 cp docker_images.yml $DST/build
-cp manifest.xml $DST/.repo/manifests/default.xml
 cp mcord-oai.yml $DST/orchestration/profiles/mcord/
+
 cp mcord-oai-physical.yml $DST/orchestration/profiles/mcord/podconfig/
-cp public-net.yaml.j2 $DST/build/platform-install/roles/cord-profile/templates/
-cp oai-net.yaml.j2 $DST/build/platform-install/roles/cord-profile/templates/
-cp mcord-oai-services.yml.j2 $DST/build/platform-install/roles/cord-profile/templates/
-cp mcord-oai-service-graph.yml.j2 $DST/build/platform-install/roles/cord-profile/templates/
+cp public-net.yaml.j2 $DST/orchestration/profiles/mcord/templates/
+cp oai-net.yaml.j2 $DST/orchestration/profiles/mcord/templates/
+cp mcord-oai-services.yml.j2 $DST/orchestration/profiles/mcord/templates/
+cp mcord-oai-service-graph.yml.j2 $DST/orchestration/profiles/mcord/templates/
 
 # Use custom version of services instead official
 cd ~/cord/orchestration/xos_services
@@ -24,5 +24,7 @@ for var in "vbbu" "vhss" "vmme" "vspgwc" "vspgwu"; do
     git remote add opencord https://github.com/aweimeow/$var.git;
     git checkout $BRANCH;
     git checkout -b $CORD_VER;
+    mkdir .git/refs/remotes/opencord/;
+    echo $(git log --format="%H" -n 1) > .git/refs/remotes/opencord/$CORD_VER;
     cd ..;
 done
