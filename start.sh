446d82086fa8d96b78795ee745cb194f97b9faf1
Wei-Yu CHen <aweimeow.tw@gmail.com>
2018-06-14 23:31:22 +0800

    Done all service, but epc-service still WIP.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CORD_VER='cord-5.0'
-BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+BRANCH='master' #$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 DST=~/cord
 
 # Copy new files into our target
@@ -10,16 +10,18 @@ cp mcord-oai.yml $DST/orchestration/profiles/mcord/
 
 cp mcord-oai-physical.yml $DST/orchestration/profiles/mcord/podconfig/
 cp mcord-oai-services.yml.j2 $DST/orchestration/profiles/mcord/templates/
+cp mcord-oai-address-manager.yml.j2 $DST/orchestration/profiles/mcord/templates/
 cp mcord-oai-test-playbook.yml $DST/orchestration/profiles/mcord/test/
 
 # Use custom version of services instead official
 cd ~/cord/orchestration/xos_services
 
-for var in "vbbu" "epc-service" "vhss" "vmme" "vspgwc" "vspgwu"; do
+for var in "vbbu" "vhss" "vmme" "vspgwc" "vspgwu"; do
     rm -rf $var;
     git clone https://github.com/aweimeow/$var.git $var;
     cd $var;
     git remote add opencord https://github.com/aweimeow/$var.git;
+    git remote add dev git@github.com:aweimeow/$var.git;
     git checkout $BRANCH;
     git checkout -b $CORD_VER;
     mkdir .git/refs/remotes/opencord/;
